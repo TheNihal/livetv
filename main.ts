d7434e58ee57b7c68939255890301dcbbcae066d
Nihal <30835109+TheNihal@users.noreply.github.com>
2026-07-10 01:17:42 +0600

    A

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+Deno.serve(async (req: Request) => {
+    const url = new URL(req.url);
+    if (url.pathname === "/iptv.txt") {
+        return await serveFile(req, "./iptv.txt");
+    }
+    return await serveFile(req, "./index.html");
 });
